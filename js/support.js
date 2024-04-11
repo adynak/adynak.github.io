d636c4c2cabf9a58341bd204177c06ed9b096303
Al Dynak <adynak@gmail.com>
2024-04-11 14:46:04 -0700

    release

--- js/support.js
+++ js/support.js
@@ -1,17 +1,12 @@
-		function getTutorialPage (tutorialPage) {
-			let pageHtml = tutorialPage.concat(".html");
-  			fetch(pageHtml)
-  			.then(res => res.text())
-  			.then(txt => document.getElementById("demo").innerHTML = txt);
-		}
+function getTutorialPage (tutorialPage) {
+	let pageHtml = tutorialPage.concat(".html");
+		fetch(pageHtml)
+		.then(res => res.text())
+		.then(txt => document.getElementById("demo").innerHTML = txt);
+}
 
-		function rewind() {
-			var mediaElement = document.getElementById("video"); 
-    		mediaElement.pause(); 
-    		mediaElement.currentTime = 0;
-		}
-
-		function sfsg(){
-			console.log("SFSG")
-		}
-
+function rewind() {
+	var mediaElement = document.getElementById("video"); 
+	mediaElement.pause(); 
+	mediaElement.currentTime = 0;
+}
