028efc34e548bb511f6365c48d2061ece6480112
adynak <adynak@gmail.com>
2025-03-08 12:24:13 +0100

    initial commit
    Salamanca Connectionss Game

---
+++ conexiones/puzzles.js
@@ -0,0 +1,7 @@
+let json = [
+    {"easy":["FAMILIA FEMENINA",["tia","abuela","hermana","madre"]],"medium":["FAMILIA MASCULINA",["tio","abuelo","hemarno","padre"]],"harder":["AMIGO MASCULINA",["amigo","compañero","amigote","chico"]],"difficult":["AMIGA FEMENINA",["amiga","compañera","amigota","chica"]]},
+    {"easy":["PRONOMBRES",["YO","TÚ","ÉL","ELLA"]],"medium":["VERBO SER",["SOY","ERES","ES","SOMOS"]],"harder":["VERBO VER",["VEO","VES","VE","VEMOS"]],"difficult":["VERBO IR",["VOY","VAS","VAMOS","VA"]]},
+    {"easy":["PRONOMBRES",["YO","TÚ","ÉL","ELLA"]],"medium":["VERBO SER",["SOY","ERES","ES","SOMOS"]],"harder":["VERBO VER",["VEO","VES","VE","VEMOS"]],"difficult":["VERBO IR",["VOY","VAS","VAMOS","VA"]]},
+    {"easy":["PRONOMBRES",["YO","TÚ","ÉL","ELLA"]],"medium":["VERBO SER",["SOY","ERES","ES","SOMOS"]],"harder":["VERBO VER",["VEO","VES","VE","VEMOS"]],"difficult":["VERBO IR",["VOY","VAS","VAMOS","VA"]]},
+    {"easy":["LOS OJOS",["AZULES","GRISES","VERDES","MARRONES"]],"medium":["ANIMALES",["PERRO","CABALLO","ÁGUILA","MONO"]],"harder":["SALUDOS",["CABALLERO","MAESTRA","HIDALGO","DON"]],"difficult":["COSAS QUE SON AZULES",["MAR","CIELO","OJOS","ARÁNDANOS"]]}
+];
