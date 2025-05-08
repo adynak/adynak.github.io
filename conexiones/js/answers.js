cf2ba2ca4142d400b2cb6f3c6928ea970d9d7bff
Al Dynak <adynak@gmail.com>
2025-05-08 16:42:20 -0700

    add previous button

--- conexiones/js/answers.js
+++ conexiones/js/answers.js
@@ -23,6 +23,41 @@
   document.getElementById("clue31").value = z3[1][1];
   document.getElementById("clue32").value = z3[1][2];
   document.getElementById("clue33").value = z3[1][3];
+
+  const previousButton = document.getElementById("previousButton");
+
+  previousButton.addEventListener("click", function() {
+    puzzleID--;
+  if (puzzleID == -1) {
+    puzzleID = json.length - 1;
+  }
+  var z0 = json[puzzleID].easy;
+  var z1 = json[puzzleID].medium;
+  var z2 = json[puzzleID].harder;
+  var z3 = json[puzzleID].difficult;
+  document.getElementById("easy").value = z0[0];
+  document.getElementById("clue00").value = z0[1][0];
+  document.getElementById("clue01").value = z0[1][1];
+  document.getElementById("clue02").value = z0[1][2];
+  document.getElementById("clue03").value = z0[1][3];
+  document.getElementById("medium").value = z1[0];
+  document.getElementById("clue10").value = z1[1][0];
+  document.getElementById("clue11").value = z1[1][1];
+  document.getElementById("clue12").value = z1[1][2];
+  document.getElementById("clue13").value = z1[1][3];
+  document.getElementById("harder").value = z2[0];
+  document.getElementById("clue20").value = z2[1][0];
+  document.getElementById("clue21").value = z2[1][1];
+  document.getElementById("clue22").value = z2[1][2];
+  document.getElementById("clue23").value = z2[1][3];
+  document.getElementById("difficult").value = z3[0];
+  document.getElementById("clue30").value = z3[1][0];
+  document.getElementById("clue31").value = z3[1][1];
+  document.getElementById("clue32").value = z3[1][2];
+  document.getElementById("clue33").value = z3[1][3];
+  document.getElementById("newPuzzle").innerHTML = "";
+  });
+
   const nextButton = document.getElementById("nextButton");
   nextButton.addEventListener("click", function() {
   puzzleID++;
